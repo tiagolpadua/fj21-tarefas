7fe6acf815f56b1ecc8d9d58add9f3473254c019
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-03-20 23:43:31 -0300

    ex 14.8

--- src/br/com/caelum/tarefas/modelo/Tarefa.java
+++ src/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -2,11 +2,20 @@ package br.com.caelum.tarefas.modelo;
 
 import java.util.Calendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity
 public class Tarefa {
+    
+    @Id
+    @GeneratedValue
 	private Long id;
 	
 	@Size(min=5)
@@ -14,6 +23,7 @@ public class Tarefa {
 	
 	private boolean finalizado;
 	
+	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern="dd/MM/yyyy")
 	private Calendar dataFinalizacao;
 
